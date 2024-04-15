972f8b200e3695c4636fccefd8507bb1dddbc476
Assawf <93886827+Assawf@users.noreply.github.com>
2024-04-15 16:37:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function fetchData() {
-  fetch('/temperature')
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('temperature').innerText = `Temperature: ${data.temperature} °C`;
-    })
-    .catch(error => console.error('Error fetching data:', error));
+  fetch('/nangluong/data') // Sử dụng đường dẫn cụ thể là /nangluong/data
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('temperature').innerText = `Temperature: ${data.temperature} °C`;
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
 }
 
 setInterval(fetchData, 5000); // Lấy dữ liệu mỗi 5 giây
